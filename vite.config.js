1bc262bab4a3a45ac835ac309f04f9b4bc7ae28e
cozzani1984-beep <cozzani1984@icloud.com>
2026-08-05 10:42:58 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["apple-touch-icon.png"],
+      manifest: false,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpeg,svg}"],
+      },
+    }),
+  ],
 });
